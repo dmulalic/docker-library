4bf04b1d451753cbab0d8163ad7b0f0437a759fc
Denis Mulalic <mulalic83@gmail.com>
2022-01-19 19:36:58 +0000

    Update omnibus Dockerfile and docker-entrypoint.sh

--- omnibus/0.1/docker-entrypoint.sh
+++ omnibus/0.1/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-exec "${CATALINA_HOME}/bin/catalina.sh" run
-# exec "${KC_HOME}/bin/standalone.sh" -Djboss.socket.binding.port-offset=10
+exec "${CATALINA_HOME}/bin/catalina.sh" run &
+exec "${KC_HOME}/bin/standalone.sh" -b 0.0.0.0
